1d012fef6384bb1f26b73851bc10c12dd7fe17cf
Victor Long <nackor91@gmail.com>
2022-09-07 17:51:39 -0700

    starter model for user

---
+++ VlongFinalProject/VlongFinalProject/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace VlongFinalProject.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
