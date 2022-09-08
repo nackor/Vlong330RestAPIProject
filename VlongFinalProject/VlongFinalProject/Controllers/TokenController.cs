25a696a68bb807d2f2060c48cdf8fc50a21fdab5
Victor Long <nackor91@gmail.com>
2022-09-07 20:57:59 -0700

    stole the token code from cstructor github

---
+++ VlongFinalProject/VlongFinalProject/Controllers/TokenController.cs
@@ -0,0 +1,21 @@
+﻿using VlongFinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VlongFinalProject.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+
+        [HttpPost]
+        public dynamic Post([FromBody] TokenRequest tokenRequest)
+        {
+            var token = TokenHelper.Tokenize(tokenRequest.Password);
+            return new { Token = token };
+        }
+
+    }
+}
