7b43d55fd37d9da50b9e0de1220ea026c6a04d49
Victor Long <nackor91@gmail.com>
2022-09-07 20:32:03 -0700

    added toeknize method

---
+++ VlongFinalProject/VlongFinalProject/TokenHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VlongFinalProject
+{
+    public class TokenHelper
+    {
+       
+        public static string Tokenize(string password)
+        {
+            byte[] salt = Encoding.ASCII.GetBytes("victor");
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+
+    }   
+}
